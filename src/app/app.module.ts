d08acd074575e96515d980ee403c7bed3119ca47
illya.shagoferov <shagoferov@gmail.com>
2020-01-10 12:39:19 +0200

    set up main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
