d7345c1dd42d3c12bd6afc02c96a5b78f4d59cba
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-01-10 14:15:52 +0200

    create welcome component (yps welcome banner)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutingComponents
+    RoutingComponents,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
