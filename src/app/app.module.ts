72ff616d87b900b5ab4e8eebd0745eae84dc15d7
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-01-12 20:42:17 +0200

    generate form-field component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { FormFieldComponent } from './components/form-field/form-field.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RoutingComponents,
-    WelcomeComponent
+    WelcomeComponent,
+    FormFieldComponent
   ],
   imports: [
     BrowserModule,
