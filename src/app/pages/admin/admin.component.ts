5e25a425bbb12682e96559d6350e50067c9748d8
Redigston <kondratukktc@gmail.com>
2020-01-14 13:20:18 +0200

    add navbar into admin page

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -7,11 +7,28 @@ import { PageService } from 'src/app/services/page.service';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-
+  isCollapsed: boolean=false;
+  isCollapsed1: boolean=true;
+  isCollapsed2: boolean=true;
   constructor(private pageService: PageService) { }
 
   ngOnInit() {
     this.pageService.append(' | Admin Panel');
   }
-
-}
+ 
+  onClick(){
+    this.isCollapsed=false;
+    this.isCollapsed1=true;
+    this.isCollapsed2=true;
+  }
+  onClick1(){
+    this.isCollapsed1=false;
+    this.isCollapsed=true;
+    this.isCollapsed2=true;
+  }
+  onClick2(){
+    this.isCollapsed2=false;
+    this.isCollapsed=true;
+    this.isCollapsed1=true;
+  }
+} 
