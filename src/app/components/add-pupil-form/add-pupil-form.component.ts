b7ae89e8b6f7d862515c19732280c8c071d02e69
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-01-22 23:18:31 +0200

    generate add pupil form

---
+++ src/app/components/add-pupil-form/add-pupil-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yps-add-pupil-form',
+  templateUrl: './add-pupil-form.component.html',
+  styleUrls: ['./add-pupil-form.component.scss']
+})
+export class AddPupilFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
