6404648f38f17a332bcc855959b896684639eca3
Bohdan Borodulin <bohdan.borodulin@outlook.com>
2020-01-30 15:32:27 +0200

    Added master personal info cabinet

---
+++ src/app/components/master-cabinets/master-cabinets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yps-master-cabinets',
+  templateUrl: './master-cabinets.component.html',
+  styleUrls: ['./master-cabinets.component.scss']
+})
+export class MasterCabinetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
