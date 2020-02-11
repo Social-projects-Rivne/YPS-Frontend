4c9d6ed2fa876301c7bbffc8d1c654109208c3e0
illya.shagoferov <shagoferov@gmail.com>
2020-02-11 13:28:34 +0200

    getting class teacher is done

--- src/app/pages/teachers/teachers.component.ts
+++ src/app/pages/teachers/teachers.component.ts
@@ -1,5 +1,6 @@
 import { ITeacher } from './../../models/ITeachet';
 import { Component, OnInit } from '@angular/core';
+import { TeacherinfoService } from 'src/app/services/teacherinfo.service';
 
 @Component({
   selector: 'yps-teachers',
@@ -8,14 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeachersComponent implements OnInit {
 
+  constructor( 
+    private userService: TeacherinfoService
+  ) {}
+
   teachersData: ITeacher[] = TEACHERS_DATA;
 
   columns = [
-    { def: "Id", label: "Id" },
+    { def: "id", label: "Id" },
     { def: "firstName", label: "Name" },
     { def: "surname", label: "Surname" },
     { def: "middleName", label: "Middle name" },
-    { def: "className", label: "Class" },
+    { def: "className" , label: "Class" },
     { def: "phoneNumber", label: "Phone" },
     { def: "email", label: "Email" },
     { def: "dateOfBirth", label: "Date of birth"},
@@ -23,12 +28,12 @@ export class TeachersComponent implements OnInit {
   ];
 
   ngOnInit() {
+    this.userService.getTeachers()
+     .subscribe(data=>this.teachersData = data
+     )
   }
-
 }
+
 const TEACHERS_DATA: ITeacher[] = [
-  {Id : 1, firstName : "Kaseem", surname : "Nyssa", middleName : "Levi", className : "ROW-15", degree : "Dosent Math" , phoneNumber : "090-831-4662", email : "auctor.Mauris@amet.org", dateOfBirth : "15.10.19"},
-  {Id : 2, firstName : "Lucian", surname : "Ava", middleName : "Callum", className : "ROW-16", degree : "Trainee", phoneNumber : "021-359-6334", email : "non.dui@dolor.com", dateOfBirth : "21.08.19"},
-  {Id : 3, firstName : "Cruz", surname : "Nayda", middleName : "Yuli", className : "ROW-17", degree : "Professor of physics", phoneNumber : "011-679-2709", email : "lectus@ac.ca", dateOfBirth : "22.03.20"},
-  {Id : 4, firstName : "Mufutau", surname : "Lareina", middleName : "Cyrus", className : "ROW-18", degree : "Music teacher", phoneNumber : "068-366-9300", email : "quis@et.com", dateOfBirth : "13.09.19"}
+  {id : null, firstName : "", surname : "", middleName : "", className : "", degree : "",  phoneNumber : "", email : "", dateOfBirth : ""}
 ];
