4a95ddfb137b7aa3c12bf14952aa743717e6ec5b
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-02-13 18:06:00 +0200

    complete adding pupil form

--- src/app/components/add-pupil-form/add-pupil-form.component.ts
+++ src/app/components/add-pupil-form/add-pupil-form.component.ts
@@ -5,17 +5,21 @@ import { validationHelper } from 'src/utils/helpers/validation-helper';
 import { requiredValidator } from 'src/utils/validators/required-validator';
 import { HttpClient } from '@angular/common/http';
 import { apiUrl } from 'src/constants/urls';
+import { IClassToSelect } from 'src/app/models/IClassToSelect';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'yps-add-pupil-form',
   templateUrl: './add-pupil-form.component.html',
-  styleUrls: ['./add-pupil-form.component.scss','../../../scss/adding-forms.scss']
+  styleUrls: ['./add-pupil-form.component.scss', '../../../scss/adding-forms.scss']
 })
 export class AddPupilFormComponent implements OnInit {
   form: FormGroup;
   fields: IFormField[] = [];
+  classes: IClassToSelect[];
   formIsOpen: boolean = false;
-  @ViewChild('formRef', { static: false }) userSubFormRef: { fields: IFormField[], userSubForm: FormGroup }; 
+
+  @ViewChild('formRef', { static: false }) userSubFormRef: { fields: IFormField[], userSubForm: FormGroup };
 
   constructor(
     private formBuilder: FormBuilder,
@@ -27,6 +31,12 @@ export class AddPupilFormComponent implements OnInit {
       "user": [null],
       "classId": [null]
     });
+
+    this.http.get(`${apiUrl}/classes/getbyschool/1`)
+      .subscribe((successRes: IClassToSelect[]) => {
+        console.log("get classes", successRes);
+        this.classes = successRes;
+      });
   }
 
   toggleForm = () => this.formIsOpen = !this.formIsOpen;
@@ -39,9 +49,13 @@ export class AddPupilFormComponent implements OnInit {
     this.userSubFormRef.fields = subFormValidationResponse.fields;
 
     if (thisFormValidationResponse.isValid && subFormValidationResponse.isValid) {
-      return this.http.post(apiUrl + "/Pupils", this.form.value).subscribe((res: any) => {
-        console.log('add pupil response', res);
-      });
+      console.log(this.form.value);
+      return this.http.post(apiUrl + "/pupils", this.form.value)
+        .subscribe(
+          (successRes: any) => {
+            console.log('add pupil response', successRes);
+          }
+        );
     }
   }
 }
