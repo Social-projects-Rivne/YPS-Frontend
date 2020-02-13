9d61575e687b222591128a63e85b37a43de06089
Karonuk <losha.pisnyak42@gmail.com>
2020-02-13 12:43:21 +0200

    added token to parent service

--- src/app/services/parent.service.ts
+++ src/app/services/parent.service.ts
@@ -1,7 +1,8 @@
 import { IParent } from './../models/IParent';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import {get} from 'js-cookie';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,15 @@ export class ParentService {
   private url: string = "https://localhost:5001/api/Parent";
   constructor(private http:HttpClient) { }
 
-  get=(id:Number):Observable<IParent[]> =>{
-      return this.http.get<IParent[]>(this.url+"?id="+id);
+  get=():Observable<IParent[]> =>{
+    let token=get('token');
+    const json=JSON.parse(token);
+    const httpOptions={
+      headers:new HttpHeaders({
+        'ContentType': 'application/json',
+        'Authorization': 'Bearer'+json['token']
+      })
+    }
+      return this.http.get<IParent[]>(this.url,httpOptions);
   }
 }
