9d5b657d7019a9596afb7f3b7ec94a911e154b79
illya.shagoferov <shagoferov@gmail.com>
2020-02-14 16:06:09 +0200

    added token for getting teachers list

--- src/app/services/teacherinfo.service.ts
+++ src/app/services/teacherinfo.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ITeacher } from '../models/ITeachet';
+import { get } from 'js-cookie';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,16 @@ export class TeacherinfoService {
   
   constructor(private http: HttpClient) { }
 
+  
+
   getTeachers=():Observable<ITeacher[]>=>{
+    let token = get('token');
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer '+ token
+      })};
     return this.http.get<ITeacher[]>(this.url+this.schoolId);
   }
 }
