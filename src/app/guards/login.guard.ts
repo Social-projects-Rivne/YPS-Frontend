83758ca33f939a4a45e95d39d323aac22b5c17ac
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-02-28 22:31:05 +0200

    change the login guard

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../services/auth/auth.service';
 import { get } from 'js-cookie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private router: Router) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -16,21 +15,14 @@ export class LoginGuard implements CanActivate {
     return this.checkNav(url);;
   }
   checkNav(url: string): boolean {
-    // Store the attempted URL for redirecting
-    this.authService.redirectUrl = url;
     let token = get('token');
-    if (!token) { return true; }
-    //Redirect to user cabinet authorized user 
-    if (this.authService.hasAdminRole()) {
-      this.router.navigate(['/admin']);
-    } else if (this.authService.hasMasterRole()) {
-      this.router.navigate(['/master']);
-    } else if (this.authService.hasHeadMasterRole()) {
-      this.router.navigate(['/head-master']);
-    } else if (this.authService.hasTeacherRole()) {
-      this.router.navigate(['/teacher']);
-    }
-    //Add new redirect after adding new cabinet
+    if (!token)
+      return true;
+
+    let role = get("role");
+
+    this.router.navigate([`/${role}`]);
+
     return false;
   }
 }
