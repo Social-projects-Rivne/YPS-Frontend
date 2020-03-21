c8367ba3726fa416824846e6a29ac7a555afdd88
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-21 14:20:42 +0200

    remove the table from the pupils page and amend the classes page component

--- src/app/pages/pupils/pupils.component.ts
+++ src/app/pages/pupils/pupils.component.ts
@@ -9,22 +9,7 @@ import { PupilsService } from '../../services/pupils/pupils.service';
 })
 
 export class PupilsComponent implements OnInit {
-
   constructor(public pupilsService: PupilsService) {}
-
-  pupilsData: IPupil[];
-  isLoading: boolean = true;
-
-  columns = [
-    { def: "id", label: "Id" },
-    { def: "firstName", label: "Name" },
-    { def: "surname", label: "Surname" },
-    { def: "middleName", label: "Middle name" },
-    { def: "className", label: "Class" },
-    { def: "phoneNumber", label: "Phone" },
-    { def: "email", label: "email" },
-    { def: "dateOfBirth", label: "Date of birth" },
-  ];
 
   ngOnInit() {
     this.pupilsService.getPupils();
