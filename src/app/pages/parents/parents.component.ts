15a617d76954f3a8818cd911ede4fbf6e0bc5624
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-23 19:24:42 +0200

    get rid of the table on the pupils page

--- src/app/pages/parents/parents.component.ts
+++ src/app/pages/parents/parents.component.ts
@@ -5,26 +5,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'yps-parents',
   templateUrl: './parents.component.html',
-  styleUrls: ['./parents.component.scss', '../../../scss/cabinet-sub-pages.scss']
+  styleUrls: ['../../../scss/cabinet-sub-pages.scss']
 })
 export class ParentsComponent implements OnInit {
-
-  columns = [
-    { def: "id", label: "Id" },
-    { def: "firstName", label: "Name" },
-    { def: "surname", label: "Surname" },
-    { def: "middleName", label: "Middle name" },
-    { def: "phoneNumber", label: "Phone" },
-    { def: "email", label: "Email" },
-    { def: "workInfo", label: "Work Info" },
-    { def: "children", label: "Children" }
-  ];
-
   constructor(public parentsService: ParentsService) { }
 
   ngOnInit() {
     this.parentsService.getParents();
   }
-
 }
 
