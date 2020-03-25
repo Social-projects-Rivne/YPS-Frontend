57153facf9dcbd032592af4db1a442658acd2bdb
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-25 21:53:22 +0200

    working with the teacher item component

--- src/app/components/teacher-item/teacher-item.component.ts
+++ src/app/components/teacher-item/teacher-item.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITeacher } from 'src/app/models/ITeacher';
 
 @Component({
   selector: 'yps-teacher-item',
   templateUrl: './teacher-item.component.html',
   styleUrls: ['./teacher-item.component.scss']
 })
-export class TeacherItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class TeacherItemComponent {
+  @Input() teacher: ITeacher;
+  @Input() index: number;
 }
