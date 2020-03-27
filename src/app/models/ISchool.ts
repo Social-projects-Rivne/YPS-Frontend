9026e6d6ea356d811ea43a051e0a1bc6964b9cea
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-27 19:42:35 +0200

    get info of the current school

---
+++ src/app/models/ISchool.ts
@@ -0,0 +1,13 @@
+export interface ISchool {
+  id: number;
+  name: string;
+  shortName: string;
+  locality: string;
+  address: string;
+  email: string;
+  pupils: number;
+  teachers: number;
+  parents: number;
+  auditoriums: number;
+  disciplines: number;
+}
--- src/app/pages/school-main/school-main.component.ts
+++ src/app/pages/school-main/school-main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService } from 'src/app/services/events/event.service';
-import { IEvent } from 'src/app/models/IEvent';
+import { HttpClient } from '@angular/common/http';
+import { apiUrl } from 'src/constants/urls';
+import { ISchool } from 'src/app/models/ISchool';
+import { HttpOptionsService } from 'src/app/services/http-options/http-options.service';
 
 @Component({
   selector: 'yps-school-main',
@@ -9,10 +11,20 @@ import { IEvent } from 'src/app/models/IEvent';
 })
 export class SchoolMainComponent implements OnInit {
   url = "/GetUpcomingEventsBySchool";
-
-  constructor() { }
+  school: ISchool;
+  constructor(private http: HttpClient, private httpOptionsService: HttpOptionsService) { }
 
   ngOnInit() {
+    this.httpOptionsService.loadHeaders();
+    console.log("school ", this.school)
+    this.http.get(`${apiUrl}/schools`, this.httpOptionsService.options)
+      .subscribe(
+        (response: any) => {
+          console.log(response)
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      )
   }
-
 }
